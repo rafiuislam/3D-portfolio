bf46a53b1347663b1464f1df80a5706460120b26
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2023-03-12 16:52:47 +0600

    upto feedback section done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { SectionWrapper } from "../hoc";
+import { BallCanvas } from "./canvas";
+import { technologies } from "../constants/constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default Tech;
