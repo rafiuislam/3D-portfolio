65705105b005d73e6c8b6aac71fa585b01028b65
Rafiu Islam <md.rafiu.islam@g.bracu.ac.bd>
2023-03-14 12:06:25 +0600

    starts motion updated

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,14 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = ({ props }) => {
   const ref = useRef();
+
   const sphere = random.inSphere(new Float32Array(5000), { random: 1.2 });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 15;
+    ref.current.rotation.y -= delta / 10;
+  });
+
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
